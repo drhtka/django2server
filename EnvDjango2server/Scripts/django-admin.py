fa032419fb73015e7be4d3d0568e7acec1173fc0
drhtka <drhtka@gmail.com>
2019-08-21 20:57:42 +0300

    MarcDown_end

---
+++ EnvDjango2server/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\YandexDisk\django_project\django2server\EnvDjango2server\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ wiki/DocsServe/docs/serve/serve_index.md
@@ -0,0 +1,15 @@
+### Настройка сервера.
+
+SSH
+FTP 
+
+**1.**
+Начать работать с сервером:
+
+- установить Apache 2 и Ngingx
+
+- Mysql PosgreSql
+
+- mod wsgi
+
+- SSL
